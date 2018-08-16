93f5051f1c31bf70cf1e2cc73de67d328f06f769
Deluxateer <deluxateer@gmail.com>
2018-08-16 16:44:40 -0400

    adds primative counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({
+      count: ++this.state.count
+    });
+  }
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    return classes += (this.state.count === 0) ? "warning" : "primary";
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
